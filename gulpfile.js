763f7f48bc78797e6e36b14f13dc2baac874cbbc
Artem Andreev <oiva.terho@gmail.com>
2022-07-12 03:30:36 +0500

    header
    + added assets support to gulp
    + added fonts
    + added bootstrap-reboot
    + added favicon

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,16 @@ import {deleteAsync} from 'del';
 import htmlmin from 'gulp-htmlmin';
 import csso from 'gulp-csso';
 import GulpUglify from 'gulp-uglify';
+import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
-import compSass from 'sass';
-import sassGlob from 'gulp-sass-glob';
 import autoprefixer from 'gulp-autoprefixer';
 import size from 'gulp-size';
 import connect from 'gulp-connect';
+import imagemin from "gulp-imagemin";
+
 // Remove dist folder
 const clear = async function () {
-  return await deleteAsync(['./dist/']);
+  return await deleteAsync(['dist']);
 };
 
 // HTML task
@@ -19,21 +20,21 @@ gulp.task('html', function () {
   return gulp
     .src('src/*.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(size({ title: 'index.html' }))
     .pipe(gulp.dest('dist'))
     .pipe(connect.reload())
 });
 
 // SASS task
-const gsass = gulpSass(compSass);
+const sass = gulpSass(dartSass);
 gulp.task('sass', function () {
   return gulp
-    .src('./src/sass/**/*.{sass,scss}', { sourcemaps: true })
-    .pipe(sassGlob())
-    .pipe(gsass())
+    .src('src/sass/**/*.{sass,scss}', { sourcemaps: true })
+    .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(csso())
-    .pipe(size({ title: 'style.min.css' }))
-    .pipe(gulp.dest('./dist/', { sourcemaps: true }))
+    .pipe(size({ title: 'style.css' }))
+    .pipe(gulp.dest('dist', { sourcemaps: true }))
     .pipe(connect.reload())
 });
 
@@ -42,26 +43,37 @@ gulp.task('js', function () {
   return gulp
     .src('src/js/*.js')
     .pipe(GulpUglify())
+    .pipe(size({ title: 'script.js' }))
     .pipe(gulp.dest('dist'))
+    .pipe(connect.reload())
+});
+
+// Assets copy task
+gulp.task('assets', function () {
+  return gulp
+    .src('src/assets/**/*.*')
+    .pipe(imagemin())
+    .pipe(size({ title: 'assets' }))
+    .pipe(gulp.dest('dist/public'))
     .pipe(connect.reload())
 });
 
 // Connect task
 gulp.task('connect', function () {
   connect.server({
-    root: './dist',
+    root: 'dist',
     livereload: true,
   });
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['./src/*.html'], gulp.series('html'));
-  gulp.watch(['./src/sass/**/*.{sass, scss}'], gulp.series('sass'));
-  gulp.watch(['./src/js/*.js'], gulp.series('js'));
+  gulp.watch(['src/*.html'], gulp.series('html'));
+  gulp.watch(['src/sass/**/*.{sass, scss}'], gulp.series('sass'));
+  gulp.watch(['src/js/*.js'], gulp.series('js'));
 });
 
 // Build
-const build = gulp.series(clear, gulp.parallel('html', 'sass', 'js'));
+const build = gulp.series(clear, gulp.parallel('html', 'sass', 'js', 'assets'));
 
 const dev = gulp.series(build, gulp.parallel('connect', 'watch'));
 
