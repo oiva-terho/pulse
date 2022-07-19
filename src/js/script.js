98ec09aff4bd083b13ef102e4d5b635fbcc5d637
Artem Andreev <oiva.terho@gmail.com>
2022-07-19 20:11:56 +0500

    cards and cards side switch

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,15 @@
-function helloWorld() {
-  console.log('hello world')
-}
-helloWorld();
+const tabs = document.querySelectorAll('.catalog__panel')
+tabs.forEach(tab => tab.addEventListener('click', (e) => {
+  if (e.target.nodeName === 'A') {
+    e.preventDefault();
+    console.log(e.target.parentNode);
+    if (e.target.parentNode.nextSibling) {
+      e.target.parentNode.classList.toggle('catalog-item__content_active');
+      e.target.parentNode.nextSibling.classList.toggle('catalog-item__list_active');
+    } 
+    if (e.target.parentNode.previousSibling){
+      e.target.parentNode.classList.toggle('catalog-item__list_active');
+      e.target.parentNode.previousSibling.classList.toggle('catalog-item__content_active');
+    }
+  }
+}))
