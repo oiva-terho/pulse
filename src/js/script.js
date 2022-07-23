a1960553d656bab455b322a83a322a380c66939a
Artem Andreev <oiva.terho@gmail.com>
2022-07-23 22:21:44 +0500

    order buttons logic
    + styles bug fixes
    + fix scroll when modal is opened

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,8 @@
 // Tabi item sides switch
 const tabs = document.querySelectorAll('.catalog__panel')
 tabs.forEach(tab => tab.addEventListener('click', (e) => {
-  if (e.target.nodeName === 'A') {
+  if (e.target.classList.contains('catalog-item__link')) {
     e.preventDefault();
-    console.log(e.target.parentNode);
     if (e.target.parentNode.nextSibling) {
       e.target.parentNode.classList.toggle('catalog-item__content_active');
       e.target.parentNode.nextSibling.classList.toggle('catalog-item__list_active');
@@ -39,13 +38,32 @@ document.querySelectorAll("a[href^='#']").forEach(function(link) {
 });
 
 // Modal windows
+const body = document.querySelector('body');
+// Consultation
 const openConsultationButtons = document.querySelectorAll('[data-modal="consultation"]');
 const consultationModal = document.getElementById('consultation');
 openConsultationButtons.forEach(button => button.addEventListener('click', () => {
   consultationModal.showModal();
+  body.style.overflow='hidden'
 }))
 consultationModal.addEventListener('click', (e) => {
   if (!e.target.closest('.modal__wrapper')) {
     consultationModal.close();
+    body.style.overflow='auto'
+  }
+})
+
+// Buy
+const openOrderButtons = document.querySelectorAll('.button_cat-item');
+const orderModal = document.getElementById('order');
+openOrderButtons.forEach(button => button.addEventListener('click', () => {
+  orderModal.showModal();
+  body.style.overflow='hidden'
+}))
+orderModal.addEventListener('click', (e) => {
+  if (!e.target.closest('.modal__wrapper')) {
+    console.log('hit');
+    orderModal.close();
+    body.style.overflow='auto'
   }
 })
